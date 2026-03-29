3dbe150cdedf9fa7de3c30a278ef07c441af6e20
Hanilutfiazzahra <hanilutfiazzahraa@gmail.com>
2026-03-29 22:51:14 +0700

    menambahkan class DataGeneric

---
+++ TP_MOD05_103022400095/DataGeneric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MOD05_103022400095
+{
+    internal class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+}
--- TP_MOD05_103022400095/Program.cs
+++ TP_MOD05_103022400095/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MOD05_103022400095;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("103022400095");
+        data.PrintData();
+    }
+}
