00fce8f5eed132ada29d82b00f70702c41ee891f
Hanilutfiazzahra <hanilutfiazzahraa@gmail.com>
2026-03-29 22:44:45 +0700

    menambahkan HaloGeneric

--- TP_MOD05_103022400095/Program.cs
+++ TP_MOD05_103022400095/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MOD05_103022400095;
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<string>("Hani");
+    }
+}
